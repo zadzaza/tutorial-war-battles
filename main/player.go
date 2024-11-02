2ce7b1cae690140cbec0004e18cff03f8c9ca998
zadzaza <85344142+zadzaza@users.noreply.github.com>
2024-11-03 00:56:28 +0300

    git add

---
+++ main/player.go
@@ -0,0 +1,21 @@
+components {
+  id: "player"
+  component: "/res/player.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"player-down\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/res/sprites.atlas\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "rocketfactory"
+  type: "factory"
+  data: "prototype: \"/main/rocket.go\"\n"
+  ""
+}
